9df71b378bc5ed733016fcb190d39ed60ae808b9
Neil Ludban <neil.ludban@gmail.com>
2025-07-02 09:25:43 -0400

    Minimal react+next and fastapi running in containers.

---
+++ backend/pyaicam-api/main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+
+@app.get("/pyaicam-api/")
+async def get_root_api():
+    """[summary]
+    Test EP Summary
+
+    [description]
+    Description of Test EP
+    """
+    return [{"name": "cook"}, {"name": "pipsi"}]
+
+#--#
